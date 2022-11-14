c130444c8bd8b52ef7f9decfffcd0125f0a0aaf5
Arnaud Courtès <arnaud@courtes.fr>
2022-11-14 09:48:58 +0100

    Updated dependency version for Mockito and improved tests by adding a test with mock

--- src/test/java/fr/arolla/MyClassTest.java
+++ src/test/java/fr/arolla/MyClassTest.java
@@ -1,6 +1,7 @@
 package fr.arolla;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,5 +14,15 @@ public class MyClassTest {
         final var result = sut.isOne();
 
         assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void test_with_mock() {
+        final var sut = Mockito.mock(MyClass.class);
+        Mockito.when(sut.isOne()).thenReturn(8);
+
+        final var result = sut.isOne();
+
+        assertThat(result).isEqualTo(8);
     }
 }
